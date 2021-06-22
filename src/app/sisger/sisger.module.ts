297747d34ba6212b12faf3a1fc9ccb268be02433
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-06-22 13:46:58 -0500

    add login router

---
+++ src/app/sisger/sisger.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SisgerRoutingModule } from './sisger-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SisgerRoutingModule
+  ]
+})
+export class SisgerModule { }
